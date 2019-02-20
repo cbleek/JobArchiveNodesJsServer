cd3e70a79eb0484e6b08541ead346dfea376c65a
sjhumili <sjkunai@outlook.com>
2019-02-20 05:11:02 -0800

    Signup api upgrade...

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -1,21 +1,50 @@
 import firebase from '../firebase';
+import token from '../services/token';
+
 export default {
 	signup: (req, res, next) => {
 		const { username, email, password } = req.body;
-		console.log(req.body);
-		console.log(username);
-		console.log(email);
-		console.log(password);
 		if (!username || !email || !password) {
 			return res
 				.status(422)
 				.send({ error: 'User information invalid' });
 		}
 		const db = firebase.firestore();
-		db.collection('users').add(req.body);
-		return res
-			.status(200)
-			.send({ token: 'token....' });
+		let flag = 1;
+		return new Promise((resolve, reject) => {
+			db.collection("users").get()
+				.then((querySnapshot) => {
+					querySnapshot.forEach((doc) => {
+						console.log(doc.data());
+						if (doc.data().email == email) {
+							console.log('Found existing email');
+							flag = 0;
+						}
+						console.log('after email compare');
+					});
+					//Email is using
+					if (flag == 0) {
+						console.log('exist');
+						return resolve(res
+							.status(422)
+							.send({ error: 'Email is in use' }));
+					}
+
+					db.collection('users').add(req.body)
+						.then(docRef => {
+							console.log();
+							console.log(`added id: ${docRef.id}`);
+							let confirmtoken = token.generateToken(docRef.id);
+							console.log(confirmtoken);
+							return resolve(res
+								.status(200)
+								.send({ token: confirmtoken }));
+						})
+						.catch(err => { reject(err); })
+					// console.log(db.collection('users').add(req.body))
+				})
+				.catch(err => { reject(err); })
+		})
 	},
 	signin: (req, res, next) => {
 	},
--- src/services/token.js
+++ src/services/token.js
@@ -2,17 +2,18 @@ import jwt from 'jwt-simple';
 import config from '../config';
 
 export default {
-	// generateToken: function (user) {
-	//     const timeStamp = new Date().getTime();
-	//     const payload = {
-	//         sub: user.id,
-	//         iat: timeStamp
-	//     }
-	//     return jwt.encode(payload, config.jwt_secret);
-	// },
-	// verifyToken: function (token, cb) {
-	//     const decode = jwt.decode(token, config.jwt_secret)
-	//     if (!decode) return cb({ error: 'Token is not verified.' });
-	//     cb(null, decode);
-	// }
+	generateToken: function (userid) {
+		console.log('generate...');
+		const timeStamp = new Date().getTime();
+		const payload = {
+			sub: userid,
+			iat: timeStamp
+		}
+		return jwt.encode(payload, config.jwt_secret);
+	},
+	verifyToken: function (token, cb) {
+		const decode = jwt.decode(token, config.jwt_secret)
+		if (!decode) return cb({ error: 'Token is not verified.' });
+		cb(null, decode);
+	}
 }
