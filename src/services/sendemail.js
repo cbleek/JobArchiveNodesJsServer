ab6f3e06ff704e49b84381fb22358e11a71d360f
sjhumili <sjkunai@outlook.com>
2019-02-21 06:17:47 -0800

    Remove source

--- src/services/sendemail.js
+++ src/services/sendemail.js
@@ -5,15 +5,7 @@ export default {
 		console.log('We are going to send confirm email to you...')
 		console.log(token);
 		console.log(email);
-		var transporter = nodemailer.createTransport({
-			host: "posty.cross-solution.de",
-			port: 465,
-			secure: true,
-			auth: {
-				user: "api@yawik.org",
-				pass: "hzqB1I1g0ua6"
-			}
-		});
+
 		transporter.verify(function (error, success) {
 			if (error) {
 				console.log(error);
